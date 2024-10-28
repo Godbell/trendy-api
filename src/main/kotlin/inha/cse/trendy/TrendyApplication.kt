b27b1e6a1618533bb4d270e7b356268ad6a7b193
Godbell <12191579@inha.edu>
2024-10-29 00:37:16 +0900

    initial

---
+++ src/main/kotlin/inha/cse/trendy/TrendyApplication.kt
@@ -0,0 +1,11 @@
+package inha.cse.trendy
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class OrtApplication
+
+fun main(args: Array<String>) {
+	runApplication<OrtApplication>(*args)
+}
---
+++ src/test/kotlin/inha/cse/trendy/TrendyApplicationTests.kt
@@ -0,0 +1,13 @@
+package inha.cse.trendy
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class TrendyApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
